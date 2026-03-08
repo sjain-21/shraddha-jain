32c3b9d420420fc167a609cfb2946f10b120a6fc
sjain-21 <84704189+sjain-21@users.noreply.github.com>
2026-03-08 14:39:10 -0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/shraddha-jain/",
   server: {
     host: "::",
     port: 8080,
